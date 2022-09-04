e5ce1b77e492df4b5bd3f5f7df23ad3a12adc8b8
Franco Risma <email>
2022-09-04 16:37:25 -0300

    Working

--- ia-tp2.py
+++ ia-tp2.py
@@ -8,6 +8,21 @@ class Event:
         self.output = output  # Expected output given inputs
 
 
+def xor_work():
+    # 0 | 0 | 0
+    # 0 | 1 | 0
+    # 1 | 0 | 0
+    # 1 | 1 | 1
+    events = [
+        Event([0, 0], 0),
+        Event([0, 1], 1),
+        Event([1, 0], 1),
+        Event([1, 1], 0)
+    ]
+
+    perceptron_work(events=events)
+
+
 def or_work():
     # 0 | 0 | 0
     # 0 | 1 | 0
@@ -19,20 +34,9 @@ def or_work():
         Event([1, 0], 1),
         Event([1, 1], 1)
     ]
-    events = [
-        Event([0, 0], 0),
-        Event([0, 1], 0),
-        Event([1, 0], 0),
-        Event([1, 1], 1)
-    ]
 
-    weights = [
-        random_weight(),
-        random_weight(),
-        random_weight()
-    ]
+    perceptron_work(events=events)
 
-    perceptron_work(events=events, weights=weights, learning_rate=0.1)
 
 def and_work():
     # 0 | 0 | 0
@@ -46,37 +50,58 @@ def and_work():
         Event([1, 1], 1)
     ]
 
+    perceptron_work(events=events)
+
+
+def perceptron_work(events):
+    learning_rate = 0.1
+
     weights = [
-        random_weight(),
-        random_weight(),
-        random_weight()
+        0,
+        0,
+        0
     ]
 
-    perceptron_work(events=events, weights=weights, learning_rate=0.1)
-
-
-def perceptron_work(events, weights, learning_rate):
     # Agregar el 1 de control a inputs
     for event in events:
-        event.inputs.append(1)
+        event.inputs.insert(0, 1)  # Add at the begining the bias which is 1
 
-    print("inital ", weights)
-    for i in range(1000):
-        learn(events=events, weights=weights, learning_rate=learning_rate)
+    print("initial weights ", weights)
+    iteration = 0
+    while True:
+    # for i in range(30000):
+        iteration += 1
+        error = learn(events=events, weights=weights, learning_rate=learning_rate)
+        if error < 0.01:
+            break
+
+    print("iteraciones", iteration)
     print("final ", weights)
+    calculate_method(events, weights)
 
 
 def learn(events, weights, learning_rate):
     for event in events:
-        for index, singleInputValue in enumerate(event.inputs):
-            x = singleInputValue * weights[index]
-            real_output = calculate_activation(x)
-            error = calculate_error(event.output, real_output)
-            print("error", error)
-            delta = calculate_delta(real_output, error)
-            delta_weight = calculate_delta_weight(learning_rate, singleInputValue, delta)
-            new_weight = calculate_adjusted_weight(weights[index], delta_weight)
+        x = calculate_x(event, weights)
+        real_output = calculate_activation(x)
+        error = event.output - real_output
+        print("error", error)
+
+        # Actualizo los pesos
+        delta = calculate_delta(real_output, error)
+        for index, single_input in enumerate(event.inputs):
+            delta_weight = learning_rate * single_input * delta
+            new_weight = weights[index] + delta_weight
             weights[index] = new_weight
+    print(weights)
+    return error
+
+
+def calculate_x(event, weights):
+    x = 0
+    for input_index, singe_input in enumerate(event.inputs):
+        x += singe_input * weights[input_index]
+    return x
 
 
 def calculate_activation(input):
@@ -86,23 +111,18 @@ def calculate_activation(input):
 def calculate_delta(real_output, error):
     return real_output * (1 - real_output) * error
 
-
-def calculate_error(expected_output, real_output):
-    return pow((expected_output - real_output), 2)
-
-
-def calculate_delta_weight(learning_rate, input, delta):
-    return learning_rate * input * delta
-
-
-def calculate_adjusted_weight(current_weight, delta_weight):
-    return current_weight + delta_weight
-
-
 def random_weight():
     return random.uniform(-1, 1)
 
+def calculate_method(events, weights):
+    for index, event in enumerate(events):
+        print("Indice", index)
+        x = calculate_x(event, weights)
+        result = calculate_activation(x)
+        print("Para la entrada ", event.inputs, " la salida es ", result)
+
 
 if __name__ == '__main__':
-    # and_work()
+    #and_work()
     or_work()
+    # xor_work()
